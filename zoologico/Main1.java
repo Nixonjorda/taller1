254e40f50b910afd36895ced9c9f4a988c61996f
Nixonjorda <nixonangarita134@gmail.com>
2025-11-10 23:32:17 -0500

    Update Main1.java

--- zoologico/Main1.java
+++ zoologico/Main1.java
@@ -3,7 +3,6 @@ package EJER2ZOOLOGICO;
 public class Main1 {
     public static void main(String[] args) {
 
-        // Crear animales
         Leon leon = new Leon(1, "francisca", 5, 190.5, "2023-02-15", 80.0, 10);
         Elefante elefante = new Elefante(2, "fresca", 10, 5400, "2022-08-10", 1.5, "Excelente");
         cocodrilo coco = new cocodrilo(3, "patricio", 12, 500, "2021-03-12", 5, 2000);
