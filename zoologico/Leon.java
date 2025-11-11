350ba940d5b63ba1c3cd1934f4a81328274382ce
Nixonjorda <nixonangarita134@gmail.com>
2025-11-10 23:08:51 -0500

    Update Leon.java

--- zoologico/Leon.java
+++ zoologico/Leon.java
@@ -1,19 +1,21 @@
+package EJER2ZOOLOGICO;
+
 public class Leon extends Mamifero {
-    private double velocidadMax;
-    private int nivelDominancia;
+    public double velocidadMax;
+    public int nivelDominancia;
 
     public Leon(int id, String nombre, int edad, double peso, String fechaIngreso,
                 double velocidadMax, int nivelDominancia) {
-        super(id, nombre, "León", edad, peso, fechaIngreso, "Corto", 4, 38.5);
+        super(id, nombre, "León", edad, peso, fechaIngreso, "Corto", 4, 38.0);
         this.velocidadMax = velocidadMax;
         this.nivelDominancia = nivelDominancia;
     }
 
     public void rugirFuerte() {
-        System.out.println(nombre + " ruge con fuerza para marcar su territorio.");
+        System.out.println(nombre + " ruge fuertemente.");
     }
 
     public void cazarPresas() {
-        System.out.println(nombre + " está cazando una presa.");
-    }
+        System.out.println(nombre + " está cazando presas.");
+    }
 }
