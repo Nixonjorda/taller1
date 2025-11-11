38840e164a36d4a641cdf4d6eeaae4d2a0582f45
Nixonjorda <nixonangarita134@gmail.com>
2025-11-10 23:33:56 -0500

    Update cocodrilo.java

--- zoologico/cocodrilo.java
+++ zoologico/cocodrilo.java
@@ -4,9 +4,9 @@ public class cocodrilo extends Reptil {
     public int horasBajoAgua;
     public int fuerzaMordida;
 
-    public cocodrilo(int id, String nombre, int edad, double peso, String fechaIngreso,int temperaturaAmbiente,
+    public cocodrilo(int id, String nombre, int edad, double peso, String fechaIngreso,
                      int horasBajoAgua, int fuerzaMordida) {
-        super(id, nombre, edad, peso, fechaIngreso, temperaturaAmbiente);
+        super(id, nombre, edad, peso, fechaIngreso, "grandes", false,  70.0);
         this.horasBajoAgua = horasBajoAgua;
         this.fuerzaMordida = fuerzaMordida;
     }
